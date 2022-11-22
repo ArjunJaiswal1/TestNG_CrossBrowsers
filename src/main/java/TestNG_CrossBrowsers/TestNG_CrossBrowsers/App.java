58f92b2da6fe6e45275b41802ea784043b2a6eb1
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-11-22 10:27:10 +0545

    CrossBrowsers

---
+++ src/main/java/TestNG_CrossBrowsers/TestNG_CrossBrowsers/App.java
@@ -0,0 +1,13 @@
+package TestNG_CrossBrowsers.TestNG_CrossBrowsers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
